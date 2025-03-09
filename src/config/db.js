8b9a2794ceeaf6c370fd8bb6426b09e105747f57
Shubham Vishwakarma <shubhamvishwakarma2140@gmail.com>
2025-03-09 13:14:28 +0530

    first commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,15 @@
+require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // Get from .env
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false, // Required for Vercel PostgreSQL
   },
 });
+
+pool.connect()
+  .then(() => console.log("✅ PostgreSQL Connected!"))
+  .catch((err) => console.error("❌ DB Connection Error:", err));
 
 module.exports = pool;
