b9bb70afe2289d419978ed6b752c14989a8a990a
Shubham Vishwakarma <shubhamvishwakarma2140@gmail.com>
2025-03-09 13:22:16 +0530

    first commit

--- src/config/db.js
+++ src/config/db.js
@@ -3,9 +3,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // Required for Vercel PostgreSQL
-  },
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 pool.connect()
