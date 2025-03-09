43fda9fea5870df0670eed1e461cf832c95eeed8
Shubham Vishwakarma <shubhamvishwakarma2140@gmail.com>
2025-03-09 12:29:20 +0530

    first commit

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }, // Required for Vercel PostgreSQL
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
