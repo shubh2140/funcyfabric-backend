cc10887a9d2da03ad8a957484fefad350177590b
Shubham Vishwakarma <shubhamvishwakarma2140@gmail.com>
2025-03-11 03:45:44 +0530

    Commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,16 @@
-require("dotenv").config();
 const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 });
 
 pool.connect()
-  .then(() => console.log("✅ PostgreSQL Connected!"))
+  .then(() => console.log("✅ PostgreSQL Connected Successfully!"))
   .catch((err) => console.error("❌ DB Connection Error:", err));
 
 module.exports = pool;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,26 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors"); // ✅ Import cors
+
 const productRoutes = require("./routes/productRoutes");
 require("./config/db"); // Ensure DB connection
 
 const app = express();
 
-app.use(express.json()); // Middleware to parse JSON
+app.use(cors()); // ✅ Enable CORS for all routes
+app.use(express.json());
+
+const path = require("path");
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+});
+
+
 app.use("/api/products", productRoutes);
 
 app.get("/", (req, res) => {
